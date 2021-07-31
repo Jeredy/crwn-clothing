1daf83941e87239231870c7912ffe116d8987cbc
andre-tc <andrecomerciale@gmail.com>
2021-07-31 13:18:23 -0300

    turn homepage into components

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import App from './homepage.component';
+import App from './pages/homepage/homepage.component';
+import './App.css';
 
 ReactDOM.render(
   <React.StrictMode>
